97581afe79358e7f756cffec9ca05a14c3f32856
Sarvagra <sarvagra@Mac.lan>
2026-06-20 19:08:42 +0530

    GCD Problem

--- sb_input.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-public class sb_input {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        StringBuilder sb=new StringBuilder(sc.nextLine());
-        System.out.println(sb);
-    }
-}
--- temp.java
+++
@@ -1,9 +0,0 @@
-class temp{
-    public static void main(String[] args) {
-        char[] arr = new char[5];
-        if(arr[0]=='\u0000'){
-            System.out.println("Hi Nigga");
-        }
-        
-    }
-}
